53c4146b3d03fa62bc71da3f4a685137b95f218e
Sierra Wallace <sskwallace@gmail.com>
2020-07-25 06:47:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,12 +7,11 @@ WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
   [6,7,8],
-  [0,4,8],
-  [2,4,6],
   [0,3,6],
   [1,4,7],
   [2,5,8],
-  [0,4,8]
+  [0,4,8],
+  [2,4,6],
   ]
    def won?(board)
      WIN_COMBINATIONS.detect do |win_combination|
